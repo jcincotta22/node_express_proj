fe41cee01effee45464c3d4aff860261716fb56e
jcincotta22 <jcincotta22@gmail.com>
2017-01-27 10:03:15 -0500

    hello world

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
